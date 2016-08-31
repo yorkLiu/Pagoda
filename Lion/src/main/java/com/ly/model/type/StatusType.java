a1db018d55fe612ddf67ca385f6ff150268d1e4b
Yong Liu <yong.liu@ozstrategy.com>
2016-08-31 17:17:01 +0800

    Lion - add status type.

--- Lion/src/main/java/com/ly/model/type/StatusType.java
+++ Lion/src/main/java/com/ly/model/type/StatusType.java
@@ -2,9 +2,30 @@ package com.ly.model.type;
 
 /**
  * Created by yongliu on 8/22/16.
+ *
+ * @author   <a href="mailto:yong.liu@ozstrategy.com">Yong Liu</a>
+ * @version  08/31/2016 11:23
  */
-public enum  StatusType {
-  
+public enum StatusType {
+  //~ Enum constants ---------------------------------------------------------------------------------------------------
+
   ENABLE, DISABLED, DELETED, LOCKED, PUBLISHED, RETIRED;
-  
+
+  //~ Methods ----------------------------------------------------------------------------------------------------------
+
+  /**
+   * convert.
+   *
+   * @param   statusType  String
+   *
+   * @return  StatusType
+   */
+  public static StatusType convert(String statusType) {
+    if ((statusType == null) || statusType.trim().isEmpty()) {
+      return null;
+    }
+
+    return valueOf(statusType);
+  }
+
 }
