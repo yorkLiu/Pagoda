7d910ebff173c6e2008956a56448a3bae17bd08d
Yong Liu <yong.liu@ozstrategy.com>
2016-12-05 17:09:04 +0800

    PD-Proxy add export to xml using XStream.

--- PD-Proxy/src/main/java/com/ly/xml/Header.java
+++ PD-Proxy/src/main/java/com/ly/xml/Header.java
@@ -1,7 +1,27 @@
 package com.ly.xml;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 /**
  * Created by yongliu on 12/2/16.
  */
+
+@XStreamAlias("Header")
 public class Header {
+  private String BatchId;
+  private String SubscriberCode;
+  private String BranchCode;
+  private String ClientReferenceId;
+  private String DatetimeGenerated;
+
+  public Header() {
+  }
+
+  public Header(String BatchId, String SubscriberCode, String BranchCode, String ClientReferenceId, String DatetimeGenerated) {
+    this.BatchId = BatchId;
+    this.SubscriberCode = SubscriberCode;
+    this.BranchCode = BranchCode;
+    this.ClientReferenceId = ClientReferenceId;
+    this.DatetimeGenerated = DatetimeGenerated;
+  }
 }
