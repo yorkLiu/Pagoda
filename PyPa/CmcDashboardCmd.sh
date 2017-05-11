b4b19e8d20639730d990aecbd48324072fd6b52b
Yong Liu <yong.liu@ozstrategy.com>
2017-05-11 17:14:17 +0800

    PyPa - add execute dashboard shell

---
+++ PyPa/CmcDashboardCmd.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+###################### Shell ENV [START] #################
+# export OUTPUT_DIR='~/Downloads/JIRA/Dashboard'
+# export EMAIL_SERVER_API='test'
+# export EMAIL_SERVER_API_KEY='test'
+# export EMAIL_FROM='Dashboard Observer <dashboard_observer@ozstrategy.com>'
+# export EMAIL_TO_TECH_LEADS='mail1@mal.com'
+
+###################### Shell ENV [END] ###################
+
+export OUTPUT_DIR='~/Downloads/JIRA/Dashboard'
+export EMAIL_SERVER_API='test'
+export EMAIL_SERVER_API_KEY='test'
+export EMAIL_USER_NAME='mail@mail.com'
+export EMAIL_PWD='email_password'
+export EMAIL_FROM='Dashboard Observer <mail@mai.com>'
+
+## Config this
+export EMAIL_TO_TECH_LEADS='mail1@mal.com'
+
+
+./CmcDashboardShell.py
